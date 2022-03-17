15b23efd477387784da35ce8dbfbc1bf47cb5f01
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-16 22:52:40 -0400

    add propmt questions for the user input

--- index.js
+++ index.js
@@ -6,9 +6,65 @@ const table = require('console.table');
 // Create a connection 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3302,
+    port: 3306,
     user: 'root',
-    password: 'password234',
+    password: 'Wilmercito1997',
     database: 'employees'
 });
+
+connection.connect(err => {
+    if(err) throw err;
+    console.log('Welecome to Employee Tracker');
+    startQuestions();
+})
+
+const startQuestions =() => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name:'menu',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update employee job',
+                'Exit',
+
+            ],
+        }
+    ])
+    .then(response => {
+        switch (response.questions) {
+        case 'View all departments':
+            viewDepartment();
+            break;
+        case 'View all roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            ddDepartment();
+             break;
+        case  'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        case 'Update employee job':
+            updateEmployee();
+            break;
+        case 'Exit':
+            connection.end();
+
+        }
+    });
+};
+
 
